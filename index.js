03ce9934301d454f1b3f8fe29c6e645e6ee4e2fb
cab11918 <yu.mingh@husky.neu.edu>
2019-11-20 14:08:35 -0500

    added placeholder and onmouseover implemented

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 document.onclick = userClicked;
+
 function userClicked() {
 
   let w = window.innerWidth;
@@ -15,14 +16,31 @@ function userClicked() {
   let usUpperLeftX = 0.1347
   let usUpperLeftY = 0.3219
   let usBottomRightX = 0.3014
-  let usBottonRightY = 0.4853
+  let usBottomRightY = 0.4853
 
+  let chinaUpperLeftX = 0.6917
+  let chinaUpperLeftY = 0.32198
+  let chinaBottomRightX = 0.8417
+  let chinaBottomRightY = 0.5469
 
+  let japanUpperLeftX = 0.8722
+  let japanUpperLeftY = 0.2803
+  let japanBottomRightX = 0.8889
+  let japanBottomRightY = 0.45397
 
-  if (x / w > usUpperLeftX && x/w < usBottomRightX && y/h > usUpperLeftY && y/h < usBottonRightY) {
+  if (x / w > usUpperLeftX && x / w < usBottomRightX && y / h > usUpperLeftY
+      && y / h < usBottomRightY) {
     {
       alert("You entered United States!")
     }
+  } else if (x / w > chinaUpperLeftX && x / w < chinaBottomRightX && y / h
+      > chinaUpperLeftY && y / h < chinaBottomRightY) {
+    alert("You entered China!")
+
+  }else if (x / w > japanUpperLeftX && x / w < japanBottomRightX && y / h
+      > japanUpperLeftY && y / h < japanBottomRightY) {
+    alert("You entered Japan!")
+
   }
 }
 
